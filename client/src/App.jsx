ed309d67dfac5df3984f87d73b2926cba4e678be
Akan Ksha <akkitacker@gmail.com>
2024-07-11 13:01:38 +0530

    Your commit message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-// src/App.jsx
-import { Routes, Route } from 'react-router-dom';
-import Home from './container/Home.jsx';
+// import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import Home from './container/Home';
+import SignInn from './component/SignInn';
+import ForgotPassword from './component/ForgotPassword';
+//import './index.css'; // Make sure Tailwind CSS is imported
 
-function App() {
-  return (
+const App = () => (
+  <div className="App">
+    <nav className="bg-DGXwhite p-5 flex justify-center">
+      <ul className="flex list-none p-0 m-0">
+        <li className="mr-4 font-semibold text-DGXblack"><Link to="/home" className="text-white">Home</Link></li>
+        <li><Link to="/SignInn" className="text-white font-semibold text-DGXblack">SignInn</Link></li>
+      </ul>
+    </nav>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/SignInn" element={<SignInn />} />
+      <Route path="/ForgotPassword" element={<ForgotPassword />} />
     </Routes>
-  );
-}
+  </div>
+);
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+
+    colors: {
+      'DGXgreen': '#76B900',
+      'DGXblue': '#013D54',
+      'DGXwhite': '#FFFFFF',
+      'DGXblack': '#000000'
+    },
   },
+  
   plugins: [],
 }
