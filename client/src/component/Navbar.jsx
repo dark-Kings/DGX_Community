86eab35befeec9398c847ddd8e0d37a2bebae79a
Anshul Pant <anshulpant14@gmail.com>
2024-07-11 15:02:42 +0530

    frontend improvement

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { images } from '../constant/index.js';
 import { AiOutlineMenu } from "react-icons/ai";
@@ -11,11 +11,11 @@ const Navbar = () => {
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const handleLogin = () => {
-        // Perform login logic here
-        // After successful login, set isLoggedIn to true
-        setIsLoggedIn(true);
-    };
+    // const handleLogin = () => {
+    //     // Perform login logic here
+    //     // After successful login, set isLoggedIn to true
+    //     setIsLoggedIn(true);
+    // };
 
 
     const navLinks = [
@@ -41,9 +41,9 @@ const Navbar = () => {
         }
     ]
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
+    // const toggleDropdown = () => {
+    //     setIsOpen(!isOpen);
+    // };
     return (
         <main className=''>
             <nav className='flex justify-between px-8 item-center py-6 lg:px-24 xs:px-0 xs:mx-0'>
@@ -62,7 +62,7 @@ const Navbar = () => {
                 </div>
 
                 {/*----------sidebarMenu-------------- */}
-                <div className={clsx('fixed h-full w-screen lg:hidden bg-black/50 backdrop-blur-sm top-0 right-0 -translate-x-full transition-all',
+                <div className={clsx('fixed h-full w-screen lg:hidden bg-black/50 backdrop-blur-sm top-0 right-0 -translate-x-full transition-all z-10',
                     isSideMenuOpen && 'translate-x-0'
                 )}>
                     <section className='text-black bg-DGXgreen/10 flex flex-col absolute left-0 top-0 h-screen p-8 gap-8 z-50 w-60'>
@@ -81,13 +81,13 @@ const Navbar = () => {
 
                 <section className='flex items-center gap-6 xs:gap-1'>
                     {!isLoggedIn ? (
-                        <button
+                        <Link to="/SignInn"><button
                             type="button"
-                            onClick={handleLogin}
+                            // onClick={handleLogin}
                             className="text-white bg-DGXgreen hover:bg-DGXgreen-800 focus:ring-4 focus:outline-none focus:ring-DGXgreen-300 font-medium rounded-lg text-xl px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
                             Login
-                        </button>
+                        </button></Link>
                     ) : (
                         <img
                             src=''  // Add the user's image URL here
