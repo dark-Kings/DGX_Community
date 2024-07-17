27801cb5e096d39039e4318bd55d5ce84f5c7602
Anshul Pant <anshulpant14@gmail.com>
2024-07-17 11:07:56 +0530

    safegarding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import SignInn from './component/SignInn';
 import ForgotPassword from './component/ForgotPassword';
 import ChangePassword from './component/ChangePassword.jsx';
 import Discussion from './container/Discussion.jsx';
-import LoadPage from './component/LoadPage.jsx';
+// import LoadPage from './component/LoadPage.jsx';
 
 
 
@@ -20,11 +20,8 @@ function App() {
         <div className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-            <Route path="/LoadPage" element={<LoadPage/>} />
+            {/* <Route path="/LoadPage" element={<LoadPage/>} /> */}
             {/* <Route path="/Login" element={<Login />} /> */}
-=======
->>>>>>> 2ea2ac951bff18e566e2fa2f10187d090a7fa5e1
             <Route exact path="/VerifyEmail" element={<VerifyEmail />} />
             <Route exact path="/Register" element={<Register />} />
             <Route path="/SignInn" element={<SignInn />} />
