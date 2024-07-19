4e049b0e0485051ef6b53a0cc4b53179c518f99f
Akan Ksha <akkitacker@gmail.com>
2024-07-19 17:22:49 +0530

    Community page

--- client/src/component/DiscussionModal.jsx
+++ client/src/component/DiscussionModal.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+// import React from 'react';
+import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 
 const DiscussionModal = ({ isOpen, onRequestClose }) => {
   return (
     <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            className="modal-content"
-            overlayClassName="modal-overlay"
-        >
-            <h2 className="text-2xl font-bold">Join the Discussion</h2>
-            <button onClick={onRequestClose} className="close-button">Close</button>
-            {/* Modal content here */}
-        </Modal>
-  )
-}
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      className="modal-content"
+      overlayClassName="modal-overlay"
+    >
+      <h2 className="text-2xl font-bold">Join the Discussion</h2>
+      <button onClick={onRequestClose} className="close-button">Close</button>
+      {/* Modal content here */}
+    </Modal>
+  );
+};
 
-export default DiscussionModal
+DiscussionModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onRequestClose: PropTypes.func.isRequired,
+};
+
+export default DiscussionModal;
