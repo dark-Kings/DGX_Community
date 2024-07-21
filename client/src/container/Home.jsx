d751b39fcd995afe931149914c0a4e885e7a4851
Akan Ksha <akkitacker@gmail.com>
2024-07-21 20:53:09 +0530

    404 page

--- client/src/container/Home.jsx
+++ client/src/container/Home.jsx
@@ -69,8 +69,8 @@ const Home = () => {
                 style={{ backgroundImage: `url(${images.HeroImg})` }} >
                 <div className="absolute inset-0 bg-black opacity-50" ></div> {/* Overlay */}
                 <div className="relative mx-auto max-w-7xl px-6 lg:px-8">
-                    <div className="mb-8 flex justify-center">
-                        <p className="relative rounded-full px-4 py-1.5 text-DGXwhite leading-6  bg-DGXgreen bg-opacity-80 ring-1 ring-inset ring-gray-900/10 hover:ring-gray-900/20">
+                    <div className="mb-8 flex justify-center ">
+                        <p className="relative rounded-full px-4 py-1.5 text-DGXwhite leading-6  bg-DGXgreen bg-opacity-90 ring-1 ring-inset ring-[#111827]/10 hover:ring-[#111827]/20 hover:bg-DGXblue">
                             <a href="/SignInn" target="_blank" className="font-semibold text-lime-600 bd">
                                 <span className="absolute inset-0 "></span> Join Us Today <span>→</span>
                             </a>
@@ -85,10 +85,11 @@ const Home = () => {
                         </p>
 
                     </div>
+                    </div>
 
-                </div>
+
             </div>
-            <section className="bg-neutral-100">
+            <section className="bg-[#f5f5f5]">
                 <div className="grid grid-cols-1 lg:grid-cols-2 items-center justify-items-center gap-4 bg-DGXblue p-4">
                     {/* Text and Links Section */}
                     <div className="flex flex-col justify-center items-center p-4">
@@ -100,15 +101,15 @@ const Home = () => {
                             <div className="flex flex-col justify-center items-center relative group">
                                 <p
                                     onClick={handleRedirect}
-                                    className="text-base sm:text-lg md:text-xl lg:text-2xl text-orange-600 text-DGXwhite hover:text-DGXgreen transition-colors duration-300 cursor-pointer"
+                                    className="text-base sm:text-lg md:text-xl lg:text-2xl text-DGXwhite hover:text-DGXgreen transition-colors duration-300 cursor-pointer"
                                 >
                                     Event and Workshop
                                 </p>
                                 <div className="absolute bottom-full mb-2 hidden group-hover:flex flex-col items-center">
-                                    <div className="relative z-10 p-2 text-xs md:text-sm leading-none text-DGXwhite whitespace-no-wrap bg-black shadow-lg rounded-md">
+                                    <div className="relative z-10 p-2 text-xs md:text-sm leading-none text-DGXwhite whitespace-no-wrap bg-DGXblack shadow-lg rounded-md">
                                         Check it Out
                                     </div>
-                                    <div className="w-3 h-3 -mt-1 rotate-45 bg-black"></div>
+                                    <div className="w-3 h-3 -mt-1 rotate-45 bg-DGXblack"></div>
                                 </div>
                             </div>
                             {/* Community Ranking */}
@@ -117,10 +118,10 @@ const Home = () => {
                                     Community Ranking
                                 </p>
                                 <div className="absolute bottom-full  hidden group-hover:flex flex-col items-center">
-                                    <div className="relative z-8  text-xs md:text-sm leading-none text-DGXwhite whitespace-no-wrap bg-black shadow-lg rounded-md">
+                                    <div className="relative z-8  text-xs md:text-sm leading-none text-DGXwhite whitespace-no-wrap bg-DGXblack shadow-lg rounded-md">
                                         Wait for the next update
                                     </div>
-                                    <div className="w-2 h-2 -mt-1 rotate-45 bg-black"></div>
+                                    <div className="w-2 h-2 -mt-1 rotate-45 bg-DGXblack"></div>
                                 </div>
                             </div>
                             {/* Survey and Quizzes */}
@@ -129,25 +130,25 @@ const Home = () => {
                                     Survey and Quizzes
                                 </p>
                                 <div className="absolute bottom-full mb-2 hidden group-hover:flex flex-col items-center">
-                                    <div className="relative z-4 p-2 text-xs md:text-sm leading-none text-DGXwhite whitespace-no-wrap bg-black shadow-lg rounded-md">
+                                    <div className="relative z-4 p-2 text-xs md:text-sm leading-none text-DGXwhite whitespace-no-wrap bg-DGXblack shadow-lg rounded-md">
                                         Wait for the next update
                                     </div>
-                                    <div className="w-3 h-3 -mt-1 rotate-45 bg-black"></div>
+                                    <div className="w-3 h-3 -mt-1 rotate-45 bg-DGXblack"></div>
                                 </div>
                             </div>
                             {/* Discussion Platform */}
                             <div className="flex flex-col justify-center items-center relative group ">
                                 <p
                                     onClick={handleRedirect}
-                                    className="text-base sm:text-lg md:text-xl lg:text-2xl text-orange-600 text-DGXwhite hover:text-DGXgreen transition-colors duration-300 cursor-pointer"
+                                    className="text-base sm:text-lg md:text-xl lg:text-2xl text-DGXwhite hover:text-DGXgreen transition-colors duration-300 cursor-pointer"
                                 >
                                     Discussion Platform
                                 </p>
                                 <div className="absolute bottom-full mb-2 hidden group-hover:flex flex-col items-center">
-                                    <div className="relative z-10 p-2 text-xs md:text-sm leading-none text-DGXwhite whitespace-no-wrap bg-black shadow-lg rounded-md">
+                                    <div className="relative z-10 p-2 text-xs md:text-sm leading-none text-DGXwhite whitespace-no-wrap bg-DGXblack shadow-lg rounded-md">
                                         Be A Part Now
                                     </div>
-                                    <div className="w-3 h-3 -mt-1 rotate-45 bg-black"></div>
+                                    <div className="w-3 h-3 -mt-1 rotate-45 bg-DGXblack"></div>
                                 </div>
                             </div>
                         </div>
@@ -164,20 +165,19 @@ const Home = () => {
                 </div>
             </section>
 
-
-            <section className="bg-neutral-100">
+            <section className="bg-[#f5f5f5]">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 lg:p-8">
                     {/* Text and Button Section */}
                     <div className="flex flex-col justify-center items-center lg:items-start text-center lg:text-left">
                         <div className="text-3xl md:text-4xl lg:text-5xl font-extrabold mb-4">
-                            <span className="animate-pulse bg-gradient-to-r from-DGXgreen-500 via-DGXblack-500 to-DGXblue-500 bg-clip-text text-transparent">
+                            <span className="animate-pulse bg-gradient-to-r from-DGXgreen via-DGXblack to-DGXblue bg-clip-text text-transparent">
                                 Upcoming Events
                             </span>
                             <p className="mt-2 text-sm md:text-base lg:text-lg text-DGXblue">
                                 Attend our regular events and workshops to learn from the best in the industry. Enhance your skills and network with professionals.
                             </p>
                         </div>
-                        <button className="text-sm md:text-md lg:text-lg bg-DGXgreen text-white py-2 px-4 lg:py-2 lg:px-5 border border-DGXblue rounded-md mt-4 hover:bg-zinc-800 transition-colors duration-300">
+                        <button className="text-sm md:text-md lg:text-lg bg-DGXgreen text-DGXwhite py-2 px-4 lg:py-2 lg:px-5 border border-DGXblue rounded-md mt-4 hover:bg-[#27272a] transition-colors duration-300">
                             Enroll Now
                         </button>
                     </div>
@@ -198,7 +198,7 @@ const Home = () => {
                             className="absolute top-1/2 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none transform -translate-y-1/2"
                             onClick={prevSlide}
                         >
-                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50">
+                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-DGXwhite/30 group-hover:bg-DGXwhite/50">
                                 <svg
                                     className="w-4 h-4 text-DGXwhite"
                                     aria-hidden="true"
@@ -222,7 +222,7 @@ const Home = () => {
                             className="absolute top-1/2 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none transform -translate-y-1/2"
                             onClick={nextSlide}
                         >
-                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50">
+                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-DGXwhite/30 group-hover:bg-DGXwhite/50">
                                 <svg
                                     className="w-4 h-4 text-DGXwhite"
                                     aria-hidden="true"
@@ -245,11 +245,11 @@ const Home = () => {
                 </div>
             </section>
 
-            <div className="bg-DGXgray py-24 sm:py-32 ">
+            <div className="bg-DGXgray/50 py-24 sm:py-32 ">
                 <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
                     <div className="max-w-2xl">
-                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
-                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                        <h2 className="text-3xl font-bold tracking-tight text-[#111827] sm:text-4xl">Meet our leadership</h2>
+                        <p className="mt-6 text-lg leading-8 text-[#4b5563]">
                             Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper
                             suspendisse.
                         </p>
@@ -260,8 +260,8 @@ const Home = () => {
                                 <div className="flex items-center gap-x-6">
                                     <img alt="" src={person.imageUrl} className="h-16 w-16 rounded-full" />
                                     <div>
-                                        <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
-                                        <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
+                                        <h3 className="text-base font-semibold leading-7 tracking-tight text-[#111827]">{person.name}</h3>
+                                        <p className="text-sm font-semibold leading-6 text-[#4f46e5]">{person.role}</p>
                                     </div>
                                 </div>
                             </li>
@@ -271,11 +271,11 @@ const Home = () => {
             </div>
 
 
-
         </div>
-
 
     )
 }
 
 export default Home;
+
+
