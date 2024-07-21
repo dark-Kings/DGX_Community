7a69918f391c5833962e5b4ceecf0054ec1f32df
nilesh <thakurnilesh2000@gmail.com>
2024-07-21 19:46:15 +0530

    merging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,11 +13,8 @@ import ContactUs from './container/ContactUs.jsx';
 import DiscussionModal from './component/DiscussionModal.jsx';
 import Notfound from './component/Notfound.jsx';
 import ResetPassword from './component/ResetPassword.jsx';
-<<<<<<< HEAD
 import images from './constant/images.js';
-=======
 import CommunityGuidelines from './component/CommunityGuidelines.jsx';
->>>>>>> caef754f4cc33f57193a02d8af557f8db412549e
 // import LoadPage from './component/LoadPage.jsx';
 
 
